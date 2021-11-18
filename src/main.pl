1f21739477937512675fbd59ff8ebfe0141c1b5b
vincen-tho <16520229@std.stei.itb.ac.id>
2021-11-19 02:43:08 +0700

    Added main.pl

---
+++ src/main.pl
@@ -0,0 +1,49 @@
+/* include files */
+:- include('map.pl').
+
+startGame :- 
+    write('                                                          '),nl,
+    write(' :::::::::     :::     ::::    ::: :::::::::: ::::    ::: '),nl,
+    write(' :+:    :+:  :+: :+:   :+:+:   :+: :+:        :+:+:   :+: '),nl,
+    write(' +:+    +:+ +:+   +:+  :+:+:+  +:+ +:+        :+:+:+  +:+ '),nl,
+    write(' +#++:++#+ +#++:++#++: +#+ +:+ +#+ +#++:++#   +#+ +:+ +#+ '),nl,
+    write(' +#+       +#+     +#+ +#+  +#+#+# +#+        +#+  +#+#+# '),nl,
+    write(' #+#       #+#     #+# #+#   #+#+# #+#        #+#   #+#+# '),nl,
+    write(' ###       ###     ### ###    #### ########## ###    #### '),nl,
+    write('                                                          '),nl,
+    write('      Welcome to PANEN !                                  '),nl,
+    write('                                                          '),nl,
+    write('                                                          '),nl,
+    write(' ################################################################################'),nl,
+    write(' #                                  ~PANEN~                                     #'),nl,
+    write(' # 1. start  : start your adventure                                             #'),nl,
+    write(' # 2. map    : show map                                                         #'),nl,
+    write(' # 3. status : show your current status                                         #'),nl,
+    write(' # 4. w      : move up 1 step                                                   #'),nl,   
+    write(' # 5. s      : move down 1 step                                                 #'),nl,
+    write(' # 6. d      : move right 1 step                                                #'),nl,
+    write(' # 7. a      : move left 1 step                                                 #'),nl,
+    write(' # 8. help   : show command list                                                #'),nl,
+    write(' ################################################################################').
+
+help :- 
+    write(' ################################################################################'),nl,
+    write(' #                                  ~PANEN~                                     #'),nl,
+    write(' # 1. start  : start your adventure                                             #'),nl,
+    write(' # 2. map    : show map                                                         #'),nl,
+    write(' # 3. status : show your current status                                         #'),nl,
+    write(' # 4. w      : move up 1 step                                                   #'),nl,   
+    write(' # 5. s      : move down 1 step                                                 #'),nl,
+    write(' # 6. d      : move right 1 step                                                #'),nl,
+    write(' # 7. a      : move left 1 step                                                 #'),nl,
+    write(' # 8. help   : show command list                                                #'),nl,
+    write(' ################################################################################').
+
+
+:- dynamic(isRunning/1).
+start :- isRunning(_) -> write('You already started your adventure!').
+start :- \+isRunning(_), assertz(isRunning(true)), createMap.
+
+map :- isRunning(_), createMap.
+map :- \+isRunning(_) -> write('Start the game first!')
+
--- src/map.pl
+++ src/map.pl
@@ -5,122 +5,118 @@ map_size(20, 15).
 
 /* special tile */
     % water tile
-    special_tile(2, 2, 'W').
-    special_tile(2, 3, 'W').
-    special_tile(2, 4, 'W').
-    special_tile(2, 5, 'W').
-    special_tile(3, 2, 'W').
-    special_tile(3, 3, 'W').
-    special_tile(3, 4, 'W').
-    special_tile(4, 2, 'W').
-    special_tile(4, 3, 'W').
-    special_tile(5, 2, 'W').
+    specialTile(2, 2, 'W').
+    specialTile(2, 3, 'W').
+    specialTile(2, 4, 'W').
+    specialTile(2, 5, 'W').
+    specialTile(3, 2, 'W').
+    specialTile(3, 3, 'W').
+    specialTile(3, 4, 'W').
+    specialTile(4, 2, 'W').
+    specialTile(4, 3, 'W').
+    specialTile(5, 2, 'W').
 
-    special_tile(13, 10, 'W').
+    specialTile(13, 10, 'W').
 
     % house tile
-    special_tile(7, 7, 'H').
+    specialTile(7, 7, 'H').
 
     % ranch tile
-    special_tile(8, 8, 'H').
+    specialTile(8, 8, 'R').
 
     % quest tile
-    special_tile(9, 9, 'Q').
+    specialTile(9, 9, 'Q').
 
     % marketplace tile
-    special_tile(10, 10, 'M').
+    specialTile(10, 10, 'M').
 
     % digged tile DYNAMIC
 
 
 /* render left */
-write_tile(X, Y) :- map_size(_, H),
+writeTile(X, Y) :- map_size(_, H),
                     X =:= 1,
                     Y =< H,
                     write('# '),
 					NewX is X+1,
-					write_tile(NewX, Y).
+					writeTile(NewX, Y).
 
 /* render right */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X =:= W, Y =< H,
                     write('# '), nl,
                     NewY is Y+1,
-                    write_tile(1, NewY).
+                    writeTile(1, NewY).
 
 /* render top */
-write_tile(X, Y) :- map_size(W, _),
+writeTile(X, Y) :- map_size(W, _),
                     X > 1, X < W, Y =:= 1,
                     write('# '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render bottom */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y =:= H,
                     write('# '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render regular tile */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y > 1, Y < H,
-                    \+ special_tile(X, Y, _),
+                    \+ specialTile(X, Y, _),
                     write('- '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render water tile */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y > 1, Y < H,
-                    special_tile(X, Y, 'W'),
+                    specialTile(X, Y, 'W'),
                     write('o '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render house tile */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y > 1, Y < H,
-                    special_tile(X, Y, 'H'),
+                    specialTile(X, Y, 'H'),
                     write('H '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render ranch tile */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y > 1, Y < H,
-                    special_tile(X, Y, 'R'),
+                    specialTile(X, Y, 'R'),
                     write('R '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render quest tile */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y > 1, Y < H,
-                    special_tile(X, Y, 'Q'),
+                    specialTile(X, Y, 'Q'),
                     write('Q '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render marketplace tile */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y > 1, Y < H,
-                    special_tile(X, Y, 'M'),
+                    specialTile(X, Y, 'M'),
                     write('M '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 /* render digged tile */
-write_tile(X, Y) :- map_size(W, H),
+writeTile(X, Y) :- map_size(W, H),
                     X > 1, X < W, Y > 1, Y < H,
-                    special_tile(X, Y, 'D'),
+                    specialTile(X, Y, 'D'),
                     write('= '),
                     NewX is X+1,
-                    write_tile(NewX, Y).
+                    writeTile(NewX, Y).
 
 
-
-
-
-
-create_map :- write_tile(1, 1).
+createMap :- writeTile(1, 1).
