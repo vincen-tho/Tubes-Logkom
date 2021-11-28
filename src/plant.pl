70f5aacd8d968db34a32e06659546e1e1bee80b7
Cryptofal <88304292+FaLzNaufal@users.noreply.github.com>
2021-11-28 13:14:52 +0700

    mau di merge

--- src/plant.pl
+++ src/plant.pl
@@ -12,6 +12,14 @@ plantedLetter('Magic Mushroom', 'm ', 120).
 plantedLetter('Rice Seed', 'r ', 144).
 plantedLetter('Tomato Seed', 't ', 48).
 
+/* Tanaman, hasil panen, kuantitas */
+harvestResult('w ', 'Wheat', 3).
+harvestResult('c ', 'Corn', 3).
+harvestResult('p ', 'Potato', 4).
+harvestResult('m ', 'Magic Mushroom', 2).
+harvestResult('r ', 'Rice', 2).
+harvestResult('t ', 'Tomato', 2).
+
 illegalLetter('x  ').
 /* DINAMIS */
 /* plantTime digunakan untuk menyimpan data kapan tanaman ditanam dan lama waktu untuk dapat dipanen */
